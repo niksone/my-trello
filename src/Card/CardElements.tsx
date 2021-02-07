a80815906df919c214d7ca2c3157cf727865b726
Nikita <niksone123454321@gmail.com>
2021-02-07 22:22:50 +0200

    add styles to card drag

--- src/Card/CardElements.tsx
+++ src/Card/CardElements.tsx
@@ -1,8 +1,17 @@
 import styled from 'styled-components'
 
-export const CardContainer = styled.div`
+interface CardContainerProps  {
+    isOver?: boolean
+    isDragging?: boolean
+}
+
+export const CardContainer = styled.div<CardContainerProps>`
     background-color: white;
+    opacity: ${({isOver}) => isOver ? '0.3' :'1' };
     padding: 5px 10px;
     border-radius: 5px;
     margin-top:10px;
+    max-width: 280px;
+
+    cursor: pointer;
 `
