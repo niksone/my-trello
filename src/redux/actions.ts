062784fc021f23ac8383037dabb5da2eeeb1d416
Nikita <niksone123454321@gmail.com>
2021-02-07 22:24:31 +0200

    add drag card actions

--- src/redux/actions.ts
+++ src/redux/actions.ts
@@ -1,5 +1,5 @@
 import { List } from './reducer';
-export type Action = AddList | AddTask | MoveList | SetDraggedList
+export type Action = AddList | AddTask | MoveList | MoveCard | SetDraggedList | SetDraggedCard
 
 interface AddList {type: 'ADD_LIST', payload: string}
 
@@ -7,4 +7,8 @@ interface AddTask {type: 'ADD_TASK', payload: {text: string, listId: string}}
 
 interface MoveList {type: 'MOVE_LIST', payload: {hoverId: string, dragId: string}}
 
+interface MoveCard {type: 'MOVE_CARD', payload: {hoverId: string, dragId: string, hoverColumnId: string, dragColumnId: string}}
+
 interface SetDraggedList {type: 'SET_DRAGGED_LIST', payload: string}
+
+interface SetDraggedCard {type: 'SET_DRAGGED_CARD', payload: string}
