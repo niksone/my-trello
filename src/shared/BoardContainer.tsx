941bfbda9a10b466eb66bcb5162186b8b79072b6
Nikita <niksone123454321@gmail.com>
2021-02-11 00:28:33 +0200

    add board styles

---
+++ src/shared/BoardContainer.tsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+export const BoardContainer = styled.div`
+  grid-area: 'board';
+  background-color: lightblue;
+  display: flex;
+  align-items: flex-start;
+  height: 100%;
+  /* width: 100%; */
+  padding: 10px;
+  overflow-x: scroll;
+`
+
+export const BoardWrapper = styled.div`
+  
+`
