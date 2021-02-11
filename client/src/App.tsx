cdbfae7a140827df6b2330ae18a92c577beda7b2
Nikita <niksone123454321@gmail.com>
2021-02-12 00:01:29 +0200

    clear unnecessary code

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,8 @@
 import React, { useEffect, useRef } from 'react';
-import Column from './shared/Column';
 import styled from 'styled-components'
 import { GlobalStyles } from './globalStyles';
-import { data } from './data';
-import AddNewItem from './shared/AddNewItem';
-import {useDispatch, useSelector} from 'react-redux'
-// import {RootReducerType} from './redux/store'
-import { AddItemState } from './redux/reducer';
+import {useSelector} from 'react-redux'
 import { RootReducerType } from './redux/store';
-import { useDrop } from 'react-dnd';
-import CustomDragLayer from './shared/CustomDragLayer';
-import DragColumn from './shared/Column/DragColumn';
-import {FpsView} from 'react-fps'
-import { BoardContainer } from './shared/Board/BoardContainer';
 import Header from './shared/Header';
 import Board from './shared/Board';
 
@@ -25,21 +15,9 @@ const AppContainer = styled.div`
     'board';
 `
 
-
 function App() {
   const data = useSelector((state: RootReducerType) => state.addItem)
-  const ref = useRef(null)
-  // console.log(`app ${JSON.stringify(data)}`);
-  // console.log(data);
 
-  //   const [{isOver}, drop] = useDrop({
-  //     accept: "LIST",
-  //     drop: () => console.log('dropped'), 
-  //     collect: monitor => ({
-  //         isOver: !!monitor.isOver()
-  //     })
-  // })
-  // useEffect(() => {console.log(data)}, [data])
   return (
     <>
       <GlobalStyles />
