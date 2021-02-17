cf1fecdcdb59fbfe0f456aff6402543d924f1e25
Nikita <niksone123454321@gmail.com>
2021-02-17 23:40:03 +0200

    handle login

--- client/src/Auth/LoginPage.tsx
+++ client/src/Auth/LoginPage.tsx
@@ -1,14 +1,42 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState } from 'react'
 import { AuthContainer, AuthForm, AuthFormButton, AuthFormInput, AuthFormLink, AuthFormTitle } from './AuthElements'
 
 const LoginPage = () => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    
+    const handleLogin = (e: any) => {
+        e.preventDefault()
+        axios({
+            method: 'POST',
+            data: {
+                email: email,
+                password: password
+            },
+            withCredentials: true,
+            url: 'http://localhost:5050/login'
+        })
+            .then(res => console.log(res.data))
+    }
+
     return (
         <AuthContainer>
             <AuthForm>
                 <AuthFormTitle>Login</AuthFormTitle>
-                <AuthFormInput type='email' placeholder='Enter email'/>
-                <AuthFormInput type='password' placeholder='Enter password'/>
-                <AuthFormButton>Login</AuthFormButton>
+                <AuthFormInput 
+                    type='email' 
+                    placeholder='Enter email' 
+                    value={email} 
+                    onChange={e => setEmail(e.target.value)}
+                />
+                <AuthFormInput 
+                    type='password' 
+                    placeholder='Enter password'
+                    value={password} 
+                    onChange={e => setPassword(e.target.value)}
+                />
+                <AuthFormButton onClick={e => handleLogin(e)}>Login</AuthFormButton>
                 <AuthFormLink to='/register'>
                     Don`t have account yet? Register
                 </AuthFormLink>
