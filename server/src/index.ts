51edd7e1e731f5669da228cb641e2124ae54b743
Nikita <niksone123454321@gmail.com>
2021-02-17 23:42:25 +0200

    create login route vhange username to email

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,3 +1,4 @@
+import { UserI } from './Interfaces/UserInterface';
 const express = require('express')
 const cors = require('cors')
 const mongoose = require('mongoose')
@@ -8,7 +9,7 @@ const session = require('express-session')
 const bcrypt = require('bcryptjs')
 const dotenv = require('dotenv')
 
-import { Request, Response } from "express"
+import { NextFunction, Request, Response } from "express"
 
 import User from "./User"
 
@@ -28,43 +29,53 @@ mongoose.connect(link, {
     } 
     console.log('connect to mongodb')
 })
-// app.use('/login', (req, res) => {
-//     res.send({
-//         token: 'test'
-//     })
-// })
 
-app.use(cors({origin: 'http://localhost:3000', credential: true}))
+app.use(cors({origin: 'http://localhost:3000', credentials: true}))
+app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 app.use(session({
     secret: 'secretcode',
     resave: true,
     saveUninitialized: true
 }))
-app.use(cookieParser())
+app.use(cookieParser('secretcode'))
+
 app.use(passport.initialize())
 app.use(passport.session())
 
+require('./passportConfig.ts')(passport)
 // Passport
 
 
 // Routes
 
-app.post('/register', async (req: Request, res: Response) => {
-    const {username, password} = req?.body;
+app.post('/login', (req: Request, res: Response, next: NextFunction) => {
+    passport.authenticate('local', (err: Error, user: UserI, info: any) => {
+        if( err) next(err)  
+        !user
+            ? res.send('No user')
+            : req.logIn(user, (err: Error) => {
+                if(err) next(err)
+                res.send(user)
+            })
+    })(req, res, next)
+})
 
-    if(!username || !password || typeof username !== 'string' || typeof username !== 'string'){
+app.post('/register', async (req: Request, res: Response) => {
+    const {email, password} = req?.body;
+
+    if(!email || !password || typeof email !== 'string' || typeof password !== 'string'){
         res.send('Wrong values')
         return
     }
 
-    User.findOne({username}, async (err: Error, user: typeof User) => {
+    User.findOne({email}, async (err: Error, user:  UserI) => {
         if(err) throw err
         if(user) res.send('User already exist')
         if(!user){
             const hashedPassword = await bcrypt.hash(password, 10)
             const newUser = new User({
-                username,
+                email,
                 password: hashedPassword
             })
         
@@ -74,4 +85,12 @@ app.post('/register', async (req: Request, res: Response) => {
     })
 
 })
+
+
+
+// app.use((req: Request, res: Response, next: NextFunction) => {
+//     console.log('------------', req.user)
+//     next()
+// })
+
 app.listen(5050, () => console.log('app is running'))
