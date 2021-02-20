a6632403482b61fd60f803c9fbe57efe947b7f99
Nikita <niksone123454321@gmail.com>
2021-02-21 00:07:03 +0200

    add is loading, add setUser to value provider

--- client/src/Context.tsx
+++ client/src/Context.tsx
@@ -5,19 +5,37 @@ import useToken from './useToken'
 export const userContext = createContext<any>({})
 
 const Context = ({children}: PropsWithChildren<{}>) => {
-    const [user, setUser] = useState({})
+    // console.log(value);
+    // console.log(JSON.parse(value));
+    // console.log(initialValue)
+    const [user, setUser] = useState('')
+    const [isLoading, setIsLoading] = useState(true)
     
     useEffect(() => {
-        axios({
-            method: 'GET',
-            withCredentials: true,
-            url: '/user'
-        }).then(res => setUser(res.data))
-        console.log(user);            
+        const value = localStorage.getItem('user')
+        const initialValue = value !== null ? JSON.parse(value) : ''
+        if(initialValue){
+            setUser(initialValue)
+            setIsLoading(false)
+        }
+        else{
+            axios({
+                method: 'GET',
+                withCredentials: true,
+                url: '/user'
+            }).then(res => {
+                console.log(res)
+                    setUser(res.data)
+                    localStorage.setItem('user', JSON.stringify(res.data))     
+                    setIsLoading(false)
+                }
+            )
+        }
+            console.log(user);       
     }, []) 
 
     return (
-        <userContext.Provider value={user}>
+        <userContext.Provider value={{user, setUser, isLoading}}>
             {children}
         </userContext.Provider>
     )
