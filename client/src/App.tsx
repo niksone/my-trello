dac806ccc61f945173c2872ed6f1d7db926b2920
Nikita <niksone123454321@gmail.com>
2021-02-21 20:52:07 +0200

    clean code

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,8 +3,6 @@ import styled from 'styled-components'
 import { GlobalStyles } from './globalStyles';
 import {useSelector} from 'react-redux'
 import { RootReducerType } from './redux/store';
-import Header from './shared/Header';
-import Board from './shared/Board';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import RegisterPage from './Auth/RegisterPage';
 import BoardPage from './BoardPage';
@@ -12,14 +10,11 @@ import LoginPage from './Auth/LoginPage';
 import UserContext, { userContext } from './Context';
 import ProtectedRoute from './ProtectedRoute';
 import ProtectedAuthRoute from './ProtectedAuthRoute';
-import { AuthProvider } from './Context/context';
 
 
 
 function App() {
   const data = useSelector((state: RootReducerType) => state.addItem)
-  // const ctx = useContext(userContext)
-  // console.log(ctx);
 
   return (
     <>
