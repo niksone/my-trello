578b0e231f14b95f149c3424ce06400f4aa3d294
Nikita <niksone123454321@gmail.com>
2021-02-22 14:51:12 +0200

    remove unneccesary code

--- server/src/passportConfig.ts
+++ server/src/passportConfig.ts
@@ -37,37 +37,3 @@ module.exports = (passport: any) => {
     })
 }
 
-// const User = require("./User");
-// const bcrypt = require("bcryptjs");
-// const localStrategy = require("passport-local").Strategy;
-
-// module.exports = function (passport) {
-//     passport.use(
-//         new localStrategy((email, password, done) => {
-//             User.findOne({ email: email }, (err, user) => {
-//                 if (err) throw err;
-//                 if (!user) return done(null, false);
-//                 return done(null, user)
-//                 // bcrypt.compare(password, user.password, (err, result) => {
-//                 //     if (err) throw err;
-//                 //     if (result === true) {
-//                 //         return done(null, user);
-//                 //     } else {
-//                 //         return done(null, false);
-//                 //     }
-//                 // });
-//             });
-//         })
-//     );
-
-//     passport.serializeUser((user, cb) => {
-//         cb(null, user.id);
-//     });
-    
-//     passport.deserializeUser((id, cb) => {
-//         User.findOne({ _id: id }, (err, user) => {
-//         cb(err, userInformation);
-//         });
-//     });
-// };
-
