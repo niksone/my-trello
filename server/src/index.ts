7e03f84f7cde29e807472d21ec74ed4b8b328f55
Nikita <niksone123454321@gmail.com>
2021-02-22 21:37:15 +0200

    chg

--- server/src/index.ts
+++ server/src/index.ts
@@ -43,7 +43,7 @@ mongoose.connect(process.env.MONGODB_URI || link, {
 app.use(cors({origin: whitelist, credentials: true}))
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
-app.use(session({
+app.use(cookieSession({
     secret: 'secretcode',
     name: 'user',
     resave: false,
