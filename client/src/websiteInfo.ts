c139466c486a2382b0415f209d2100b53d580d54
Nikita <niksone123454321@gmail.com>
2021-02-23 00:00:09 +0200

    change

--- client/src/websiteInfo.ts
+++ client/src/websiteInfo.ts
@@ -1,5 +1,7 @@
 export const websiteInfo = {
-    serverUrl: process.env.NODE_ENV === 'development' 
-        ? 'http://localhost:5000'
-        : 'https://nikita-trello.herokuapp.com'
+    serverUrl: 
+    // process.env.NODE_ENV === 'development' 
+    //     ? 'http://localhost:5000'
+    //     : 
+        'https://nikita-trello.herokuapp.com'
 }
