d15469cec8840eb965feeb13078f79664335ad22
Nikita <niksone123454321@gmail.com>
2021-02-22 16:04:10 +0200

    add console.log response

--- client/src/Auth/LoginPage.tsx
+++ client/src/Auth/LoginPage.tsx
@@ -26,6 +26,7 @@ const LoginPage = () => {
             url: `${websiteInfo.serverUrl}/login`
         }).then(res => {
             getUser()
+            console.log(res.data);
         })
     }
 
