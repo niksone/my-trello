c34d7d1b72938012c96f613fa5f9f70a30089073
Nikita <niksone123454321@gmail.com>
2021-02-22 19:29:31 +0200

    change

--- server/src/index.ts
+++ server/src/index.ts
@@ -96,8 +96,7 @@ app.post('/register', async (req: Request, res: Response) => {
 })
 
 app.get('/user', (req: Request, res: Response) => {
-    console.log(req.session, '=/user')
-    console.log(req.isAuthenticated(), 'isAuth')
+    console.log(req.user, '=/user')
     res.send(req.session)
 })
 
