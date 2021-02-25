b0c3d1fe40a2f0dfa556e91db2d19c05ed6ce749
Nikita <niksone123454321@gmail.com>
2021-02-25 20:34:21 +0200

    rm file

--- client/src/websiteInfo.ts
+++
@@ -1,8 +0,0 @@
-export const websiteInfo = {
-    serverUrl: 
-    // process.env.NODE_ENV === 'development' 
-    //     ? 
-        'http://localhost:5000'
-        // : 
-        // 'https://nikita-trello.herokuapp.com'
-}
