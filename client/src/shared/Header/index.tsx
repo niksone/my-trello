0dd7d9b3b4aca499dae8c561d928b4e57999a22f
Nikita <niksone123454321@gmail.com>
2021-02-25 20:33:43 +0200

    implement logout

--- client/src/shared/Header/index.tsx
+++ client/src/shared/Header/index.tsx
@@ -1,13 +1,32 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useContext } from 'react'
 import { Link } from 'react-router-dom'
 import LoginPage from '../../Auth/LoginPage'
+import { userContext } from '../../Context'
 import { HeaderContainer } from './HeaderElements'
 
 const Header = () => {
+    const {user, getUser} = useContext(userContext)
+
+    const logout = async () => {
+        axios({
+            method: 'POST',
+            url: `/logout`
+        }).then(res => {
+            getUser()
+            console.log(res.data);
+        })
+    }
     return (
         <HeaderContainer>
-            <Link to='/login'>Log In</Link>
-            <Link to='/register'>register</Link>
+            {
+                user 
+                    ? <button onClick={logout}>Log Out</button>
+                    : <>
+                        <Link to='/login'>Log In </Link>
+                        <Link to='/register'> register</Link>
+                    </>
+            }
         </HeaderContainer>
     )
 }
