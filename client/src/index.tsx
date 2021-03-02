3101cfb582365639b5fa90c9305362c0ef1beb49
Nikita <niksone123454321@gmail.com>
2021-03-03 00:13:43 +0200

    rm react-dnd

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -4,16 +4,12 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {Provider} from 'react-redux'
 import { store } from './redux/store';
-import { DndProvider } from 'react-dnd';
-import { HTML5Backend } from 'react-dnd-html5-backend';
 
 ReactDOM.render(
   <Provider store={store}>
-    <DndProvider backend={HTML5Backend}>
       <React.StrictMode>
         <App />
       </React.StrictMode>
-    </DndProvider>
   </Provider>,
   document.getElementById('root')
 );
