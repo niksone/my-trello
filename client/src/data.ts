82c37bc3c9a8c4489e9416ec445884cd96eab356
Nikita <niksone123454321@gmail.com>
2021-03-02 23:59:15 +0200

    change data

--- client/src/data.ts
+++ client/src/data.ts
@@ -1,33 +1,34 @@
 export const data = {
     lists: [
         {   
-            id: '1',
+            id: '100',
             title: 'Column 1',
             tasks: [
-                {text: 'Task 1', id: '1'},
-                {text: 'Task 2', id: '2'},
+                {text: 'Task 1', id: '0'},
+                {text: 'Task 2', id: '1'},
             ]
         },
         {
-            id: '2',
+            id: '200',
             title: 'Column 2',
             tasks: [
-                {text: 'Task 1', id: '3'},
-                {text: 'Task 2', id: '4'},
-                {text: 'Task 3', id: '5'},
+                {text: 'Task 3', id: '2'},
+                {text: 'Task 4', id: '3'},
+                {text: 'Task 5', id: '4'},
             ]
         },
         {
-            id: '3',
+            id: '300',
             title: 'Column 3',
             tasks: [
-                {text: 'Task 1', id: '6'},
-                {text: 'Task 2', id: '7'},
-                {text: 'Task 3', id: '8'},
-                {text: 'Task 4', id: '9'},
+                {text: 'Task 6', id: '5'},
+                {text: 'Task 7', id: '6'},
+                {text: 'Task 8', id: '7'},
+                {text: 'Task 9', id: '8'},
             ]
         },      
     ],
+    taskIds: ['0', '1', '2', '3', '4', '5', '6', '7', '8'],
     draggedListId: '',
     draggedCardId: ''
 }
