5e75a3547dc4542216c068d7c8c514874b4801c2
Nikita <niksone123454321@gmail.com>
2021-03-03 00:02:01 +0200

    remove react-dnd

--- client/src/shared/Board/index.tsx
+++ client/src/shared/Board/index.tsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import AddNewItem from '../AddNewItem'
-import DragColumn from '../Column/DragColumn'
-import CustomDragLayer from '../CustomDragLayer'
 import { BoardContainer } from './BoardContainer'
 import {AddItemState} from '../../redux/reducer'
 import { useDispatch } from 'react-redux'
 
-interface BoardProps {
+export interface BoardProps {
     data: AddItemState
 }
 
@@ -15,25 +13,7 @@ const Board = ({data}: BoardProps) => {
 
     return (
         <BoardContainer>
-            {/* <FpsView width={100} height={100} left={0} top={0}/> */}
-            <CustomDragLayer />
-            {
-            data.lists.map(list => 
-                <DragColumn 
-                    title={list.title} 
-                    tasks={list.tasks} 
-                    id={list.id} 
-                    key={list.id}
-                    onAdd={(text: string)=>dispatch({type: 'ADD_TASK', payload: {text, listId: list.id}})}
-                />
-            )
-            }
-            <AddNewItem
-                onAdd={text => dispatch({type: "ADD_LIST", payload: text})} 
-                text='Add New List +'
-                formText='Add List'
-                item='ADDITEM'
-            />
+           
         </ BoardContainer>
     )
 }
