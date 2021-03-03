fb5f2e29378f435c3d22305fd90d7d9896b6b5e5
Nikita <niksone123454321@gmail.com>
2021-03-03 23:25:16 +0200

    fix bug + add editbutton

--- client/src/shared/Card/CardElements.tsx
+++ client/src/shared/Card/CardElements.tsx
@@ -6,6 +6,7 @@ interface CardContainerProps  {
 }
 
 export const CardContainer = styled.div<CardContainerProps>`
+    position: relative;
     background-color: white;
     /* opacity: ${({isOver}) => isOver ? '0.3' :'1' }; */
     /* transform: rotateZ(${({isDragging}) => isDragging ? '5deg' : '0'}); */
@@ -13,6 +14,22 @@ export const CardContainer = styled.div<CardContainerProps>`
     border-radius: 5px;
     margin-top:10px;
     max-width: 280px;
-
+    word-break: break-word;
     cursor: pointer;
+`
+
+export const EditButton = styled.span`
+    position: absolute;
+    top: 5px;
+    right: 5px;
+    height: 20px;
+    width: 20px;
+    border-radius: 3px;
+    display: none;
+    background-color: grey;
+    cursor: pointer;
+    
+    ${CardContainer}:hover & {
+        display: block;
+    }
 `
