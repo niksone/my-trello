3083def97f156f172a4c1691c559b87b007173dd
Nikita <niksone123454321@gmail.com>
2021-03-05 00:11:18 +0200

    create EditCard Element

---
+++ client/src/shared/Card/EditCard.tsx
@@ -0,0 +1,41 @@
+import { text } from 'express'
+import React, { useEffect, useRef } from 'react'
+import { useDispatch } from 'react-redux'
+import styled from 'styled-components'
+import ResizableTextArea from '../ResizableTextArea'
+import { CardContainer, EditButton } from './CardElements'
+
+const EditCardContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+`
+const EditCard = ({listId, taskId, text}: any) => {
+    const dispatch = useDispatch()
+    const test = useRef<HTMLSpanElement>(null)
+
+    const editCard = () => {
+        const text = test?.current?.innerText
+        dispatch({type: 'EDIT_CARD', payload: {listId, taskId, text}})
+    }
+
+    const deleteCard = () => {
+        dispatch({type: 'DELETE_CARD', payload: {listId, taskId}})
+    }
+
+    useEffect(() => {
+        // test?.current?.focus()
+        test.current && (test.current.innerText = text)
+    })
+
+    return (
+        <EditCardContainer>
+            <CardContainer>
+                <ResizableTextArea ref={test}/>
+                <EditButton style={{backgroundColor: 'red'}} onClick={deleteCard}/>
+            </CardContainer>
+            <button style={{backgroundColor: 'green'}} onClick={editCard}>submit</button>
+        </EditCardContainer>
+    )
+}
+
+export default EditCard
