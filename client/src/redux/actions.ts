dbda503495ef212ee17be203e1435ba57f2bc05e
Nikita <niksone123454321@gmail.com>
2021-03-05 00:10:11 +0200

    add delete card and edit card actions

--- client/src/redux/actions.ts
+++ client/src/redux/actions.ts
@@ -1,6 +1,7 @@
 import { List } from './reducer';
-export type Action = AddList | AddTask | MoveList | MoveCard | SetDraggedList | SetDraggedCard
-
+export type Action = AddList | AddTask | MoveList 
+| MoveCard | SetDraggedList | SetDraggedCard | EditCard
+| DeleteCard
 interface AddList {type: 'ADD_LIST', payload: string}
 
 interface AddTask {type: 'ADD_TASK', payload: {text: string, listId: string}}
@@ -18,3 +19,7 @@ interface MoveCard {type: 'MOVE_CARD',
 interface SetDraggedList {type: 'SET_DRAGGED_LIST', payload: string}
 
 interface SetDraggedCard {type: 'SET_DRAGGED_CARD', payload: string}
+
+interface EditCard {type: 'EDIT_CARD', payload: {listId: string, taskId: string, text: string}}
+
+interface DeleteCard {type: 'DELETE_CARD', payload: {listId: string, taskId: string}}
