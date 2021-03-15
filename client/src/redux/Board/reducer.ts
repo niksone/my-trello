794fa8209c988e581aa4caa8c9fb4b6a43207b76
Nikita <niksone123454321@gmail.com>
2021-03-16 01:03:55 +0200

    upd board reducer

--- client/src/redux/Board/reducer.ts
+++ client/src/redux/Board/reducer.ts
@@ -1,10 +1,12 @@
+import axios, { AxiosResponse } from 'axios';
+import { Dispatch } from 'redux';
 import {v4 as uuid} from 'uuid'
-import { boards as boardsData } from '../../data';
-import { List, Task } from '../AddItem/reducer';
+import { boardApi } from '../../api';
+import { List } from '../AddItem/reducer';
 import { BoardAction } from "./actions"
 
 export interface Board {
-    id: string,
+    _id: string,
     name: string,
     lists: List[],
     // tasks: Task[],
@@ -15,21 +17,50 @@ export interface Board {
 
 export interface BoardReducerProps {
     boards: Board[],
+    isLoading: boolean
 }
 
+export const getBoards = (user: string) => async (dispatch: Dispatch<BoardAction>) =>{
+    try {
+        // dispatch({type: 'SET_LOADING', payload: true})
+        const boards = (await boardApi.getBoards(user)).data
+        // const boards = await axios.get('/boards',{data: user})
+        console.log(boards, '==boards')
+        dispatch({type: 'SET_BOARDS', payload: boards})
+        // done()
+    } catch (error) {
+        
+    }
+}
 
+export const addBoard = (userId: string, boardName: string) => async (dispatch: Dispatch<BoardAction>) => {
+    try {
+        // await boardApi.
+        const board = (await boardApi.addBoard(userId, boardName)).data
+        dispatch({type: 'ADD_BOARD', payload: {id: board._id, name: boardName}})
+    } catch (error) {
+        
+    }
+}
+const initialState: BoardReducerProps = {
+    boards: [] as Board[],
+    isLoading: true
+}
 
-export const boardReducer = (state: BoardReducerProps = {boards: boardsData}, action: BoardAction): BoardReducerProps => {
+export const boardReducer = (state: BoardReducerProps = initialState, action: BoardAction): BoardReducerProps => {
     switch(action.type) {
-        // case 'SET_BOARD': {
-        //     console.log('reducer', action.payload)
-        //     return {...state, currentBoard: action.payload}
-        // }
+        case 'SET_LOADING': {
+            return {...state, isLoading: action.payload}
+        }
+
+        case 'SET_BOARDS': {
+            return {...state, boards: action.payload, isLoading: false}
+        }
 
         case 'ADD_BOARD': {
-            const {name} = action.payload
+            const { id, name} = action.payload
             const newBoard = {
-                id: uuid(),
+                _id: id,
                 name,
                 lists: [],
                 taskIds: [],
@@ -38,8 +69,9 @@ export const boardReducer = (state: BoardReducerProps = {boards: boardsData}, ac
             }
             state.boards.push(newBoard)
             console.log(state)
-            return {...state}
+            return {...state, isLoading: false}
         }
+
 
 
 
