c3c87615bca40708cc0468c433eacb3c4a50359c
Nikita <niksone123454321@gmail.com>
2021-03-16 00:45:54 +0200

    set user context

--- client/src/Context.tsx
+++ client/src/Context.tsx
@@ -1,30 +1,58 @@
 import axios from 'axios'
 import React, { createContext, PropsWithChildren, useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { authApi } from './api'
+import { getBoards } from './redux/Board/reducer'
 
 export const userContext = createContext<any>({})
 
 const UserContext = ({children}: PropsWithChildren<{}>) => {
-    const [user, setUser] = useState(false)
+    const [user, setUser] = useState('')
     const [isAuth, setIsAuth] = useState(false)
     const [isLoading, setIsLoading] = useState(true)
+    const dispatch = useDispatch()
         
     const getAuth = async () => {
         try {
-            const currentUser = await axios({
-                method: 'GET',
-                withCredentials: true,
-                url: `/isAuth`
-            })
-
-            console.log(currentUser);
-
-            const authData = currentUser.data
+            setIsLoading(true)
+            const authData = await authApi.isAuth()
             setIsAuth(authData)
+            if(authData){
+                const user = await authApi.getUser()
+                setUser(user)
+                console.log(user, 'user');
+            }
             setIsLoading(false)
         } catch (error) {
             console.log(error);
         }
     }
+
+    const getUser = async () => {
+        try {
+            if(isAuth){
+                const user = await authApi.getUser()
+                setUser(user)
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    // const getUserBoards = async () => {
+    //     console.log(isLoading)
+    //     try {
+    //         setIsLoading(true)
+    //         if(isAuth){
+    //             const user = await authApi.getUser()
+    //             dispatch(getBoards(user))
+    //         }
+    //         setIsLoading(false)
+
+    //     } catch (error) {
+    //         console.log(error);
+    //     }
+    // }
 
     useEffect(() => {
         getAuth()
