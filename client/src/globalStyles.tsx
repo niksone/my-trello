5e32653fbfb436314322f588e847db41d4c95739
Nikita <niksone123454321@gmail.com>
2021-03-21 17:55:33 +0200

    add variables

--- client/src/globalStyles.tsx
+++ client/src/globalStyles.tsx
@@ -1,10 +1,36 @@
-import {createGlobalStyle} from 'styled-components'
+import styled, { createGlobalStyle } from "styled-components";
 
 export const GlobalStyles = createGlobalStyle`
     *{
         padding: 0;
         margin: 0;
         box-sizing: border-box;
+        font-family: 'Lato', sans-serif;
         font-size: 62.5%;
+    }
+
+    #root{
+        --color-primary: #5E81F4;
+        --color-primary-dark: #1C1D21;
+        --color-primary-grey: #8181A5;
+        --color-primary-light: rgba(94, 129, 244, 0.1);
+        --color-outline: #F0F0F3;
+
+        --color-button-hover: #475EAB;
+        --color-button-outline-resting: #ECECF2;
+
+        --color-background: #F6F6F6;
+        
+
+        --text-h1: 3.2rem;
+        --text-h2: 2.6rem;
+        --text-h3: 2rem;
+        --text-h4: 1.8rem;
+        --text-h5: 1.6rem;
+
+        --text-caption: 1.4rem;
+        --text-regular: 1.4rem;
+
+
     }
 `
