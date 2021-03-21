b05d329a9651a43ebd14da23b8b796240a7a34c7
Nikita <niksone123454321@gmail.com>
2021-03-21 17:57:19 +0200

    add form component

---
+++ client/src/shared/FormControl/index.tsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+
+export const FormContainer = styled.form`
+    max-width: 365px;
+    padding-top: 70px;
+    padding-bottom: 115px;
+`
+
+export const FormInputsContainer = styled.div`
+    padding-bottom: 35px;
+
+    > :not(:last-child){
+        padding-bottom: 25px;
+    }
+`
