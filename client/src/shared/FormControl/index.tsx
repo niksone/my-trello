44c26ecb3eb13d4c6f25084576ab29a7cdb9d68b
Nikita <niksone123454321@gmail.com>
2021-03-22 17:51:43 +0200

    add responsiveness and form error element

--- client/src/shared/FormControl/index.tsx
+++ client/src/shared/FormControl/index.tsx
@@ -5,6 +5,13 @@ export const FormContainer = styled.form`
     max-width: 365px;
     padding-top: 70px;
     padding-bottom: 115px;
+    height: fit-content;
+
+    @media(max-width: 976px){
+        width: 325px;
+        padding-top: 20px;
+        padding-bottom: 40px;
+    }
 `
 
 export const FormInputsContainer = styled.div`
@@ -12,5 +19,32 @@ export const FormInputsContainer = styled.div`
 
     > :not(:last-child){
         padding-bottom: 25px;
+    }
+`
+
+
+interface FormErrorProps {
+    isError: boolean
+}
+export const FormError = styled.div<FormErrorProps>`
+    display: ${({isError}) => isError ? 'block' : 'none'};
+    background-color: var(--color-error);
+    color: #fff;
+    padding: 15px;
+    border-radius: 8px;
+    font-weight: bold;
+    margin-bottom: 20px;
+    animation: appear-fade 0.5s ease-in-out;
+
+    @keyframes appear-fade{
+        0%{
+            transform: translateY(100%);
+            opacity: 0;
+        }
+
+        100%{
+            transform: translateY(0);
+            opacity: 1;
+        }
     }
 `
