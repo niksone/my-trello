2894033146749f163c00b5d674c7fb5f195a8d99
Nikita <niksone123454321@gmail.com>
2021-03-22 20:32:51 +0200

    add fullheight hook

---
+++ client/src/utils/useFullHeight.ts
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from 'react'
+
+const useFullHeight = () => {
+    const [height, setHeight] = useState<number | null>()
+
+    useEffect(() => {
+        const measureHeight = () => {
+            setHeight(getHeight())
+        }
+        window.addEventListener('resize', measureHeight)
+        return () => window.removeEventListener('resize', measureHeight)
+    })
+
+    function isClient() {
+        return typeof window !== 'undefined' && typeof document !== 'undefined'
+    }
+
+    const getHeight = () => {
+        if(!isClient()) return null
+        return document.documentElement?.clientHeight || window.innerHeight
+    }
+
+    return {height, setHeight}
+}
+
+export default useFullHeight
