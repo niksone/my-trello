a6b6d868f4d3c2a9bc0361f0114756faf607e893
Nikita <niksone123454321@gmail.com>
2021-03-24 20:15:16 +0200

    add new elements

---
+++ client/src/BoardPage/BoardElements.tsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const BoardSidebar = styled.div`
+    height: 100%;
+    width: 15%;
+
+    background-color: #fff;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+
+    padding-bottom: 24px;
+`
--- client/src/shared/Buttons/index.tsx
+++ client/src/shared/Buttons/index.tsx
@@ -1,11 +1,37 @@
 import React from 'react'
-import styled, {css} from 'styled-components'
+import styled, {css, FlattenInterpolation, ThemedStyledProps} from 'styled-components'
 
+
+const shadowStylesActive = css`
+    background-color: var(--color-primary-light);
+    color: var(--color-primary);
+`
+
+const shadowStyles = css<ButtonProps>`
+    background: none;
+    border:  none;
+    color: var(${({color}) => color ? color : '--color-primary-grey'});
+    ${({active}) => active && shadowStylesActive}
+
+    &:hover{ 
+        background-color: var(--color-primary-light);
+    }
+
+    &:active{
+        ${shadowStylesActive}
+    }
+`
+
+const oulineStylesActive = css`
+    border: 1px var(--color-primary) solid;
+    color: var(--color-primary);
+`
 
 const outlineStyles = css<ButtonProps>`
     background: none;
     border:  1px var(${({color}) => color ? color : '--color-button-outline-resting'}) solid;
     color: var(${({color}) => color ? color : '--color-primary-grey'});
+    ${({active}) => active && oulineStylesActive}
 
     &:hover{ 
         border:  1px var(--color-primary) solid;
@@ -13,15 +39,22 @@ const outlineStyles = css<ButtonProps>`
     }
 
     &:active{
-        border: 1px var(--color-primary) solid;
-        color: var(--color-primary);
+        ${oulineStylesActive}
     }
 `
+
+const fillStylesActive = css`
+    background: '--color-primary';
+    color: #fff;
+`
+
 
 const fillStyles = css<ButtonProps>`
     background: var(${({background}) => background ? background : '--color-primary-light'});
     color: var(${({color}) => color ? color : '--color-primary'});
     border: none;
+    ${({active}) => active && fillStylesActive}
+
 
     &:hover {
         background: var(${({background}) => background ? background : '--color-button-hover'});
@@ -29,8 +62,7 @@ const fillStyles = css<ButtonProps>`
     }
 
     &:active{
-        background: var(${({background}) => background ? background : '--color-primary'});
-        color: #fff;
+        ${fillStylesActive}
     }
 
     &:disabled{
@@ -39,7 +71,21 @@ const fillStyles = css<ButtonProps>`
     }
 `
 
+type stylesOptions = {
+    [key: string]: FlattenInterpolation<ThemedStyledProps<ButtonProps, any>>
+}
+
+const styles: stylesOptions = {
+    outline: outlineStyles,
+    fill: fillStyles,
+    shadow: shadowStyles
+}
+
+
 const ButtonContainer = styled.button<ButtonProps>`
+    display: flex;
+    justify-content: ${({jc}) => jc ? jc : 'center'};
+    align-items: center;
     padding: 14px 29px;
     font-size: var(--text-button);
     cursor: pointer;
@@ -47,7 +93,7 @@ const ButtonContainer = styled.button<ButtonProps>`
     outline: none;
     transition: 0.2s all ease-in-out;
     width: ${({widthFill}) => widthFill ? '100%' : 'auto'};
-    ${({outline}) => outline ? outlineStyles : fillStyles};
+    ${({variant}) => variant ? styles[variant] : 'fill'};
     font-size: var(--text-regular);
 `
 
@@ -56,10 +102,14 @@ interface ButtonProps {
     background?: string,
     color?: string,
     widthFill?: boolean,
-    outline?: boolean,
+    active?: boolean,
+    variant?: 'outline' | 'fill' | 'shadow',
+    jc?: 'start' | 'center' | 'end'
 }
 
 const Button = ({children, onClick, ...rest}: React.PropsWithChildren<ButtonProps>) => {
+    rest.variant = rest.variant ? rest.variant : 'fill'
+    console.log(rest.variant)
     return (
         <ButtonContainer onClick={onClick} {...rest}>
             {children}
