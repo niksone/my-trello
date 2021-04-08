dd7cb202927039aae7e09f21c7bba1bf04fc2dc1
Nikita <niksone123454321@gmail.com>
2021-04-08 22:35:18 +0300

    upd checkbox structure

--- client/src/shared/Checkbox/index.tsx
+++ client/src/shared/Checkbox/index.tsx
@@ -1,15 +1,34 @@
-import React from 'react'
-import { CheckboxContainer, CheckboxInput, CheckboxText, Checkmark } from './CheckboxElements'
+import React, { useState } from 'react'
+import { CheckboxContainer, CheckboxInput, CheckboxText, CheckboxWrapper, Checkmark } from './CheckboxElements'
 import CheckmarkIcon from '../icons/Checkmark'
 
-const Checkbox = ({children}: React.PropsWithChildren<{}>) => {
+interface CheckboxProps {
+    checked: boolean,
+    onChange?: () => void
+}
+
+const Checkbox = ({children, checked, onChange}: React.PropsWithChildren<CheckboxProps>) => {
+    const [inputChecked, setInputChecked] = useState(checked)
+
+    const changeHandle = () => {
+        setInputChecked(!inputChecked)
+        onChange && onChange()
+    }
     return (
         <CheckboxContainer>
-            <CheckboxInput type="checkbox" name="" id=""/>
-            <Checkmark>
-                <CheckmarkIcon fill={'white'}/>
-            </Checkmark>
-            <CheckboxText>{children}</CheckboxText>
+            <CheckboxWrapper>
+                <CheckboxInput 
+                    type="checkbox" 
+                    name="" 
+                    id="" 
+                    checked={inputChecked} 
+                    onChange={changeHandle}
+                />
+                <Checkmark>
+                    <CheckmarkIcon fill={'white'}/>
+                </Checkmark>
+            </CheckboxWrapper>
+            {children && <CheckboxText checked={inputChecked}>{children}</CheckboxText>}
         </CheckboxContainer>
     )
 }
