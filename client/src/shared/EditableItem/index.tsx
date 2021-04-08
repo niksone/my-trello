a2427bbae6439feb9e7e26a88bd075c9fc147262
Nikita <niksone123454321@gmail.com>
2021-04-08 22:33:50 +0300

    change component structure

--- client/src/shared/EditableItem/index.tsx
+++ client/src/shared/EditableItem/index.tsx
@@ -3,16 +3,17 @@ import ResizableTextArea from '../ResizableTextArea'
 import { EditableItemContainer, EditButton, EditableItemWrapper, SubmitEditButton } from './EditableItemElements'
 
 interface EditableItemProps {
-    children: React.ReactChild,
+    // children: React.ReactChild,
     initialText: string,
     deleteItem: () => void,
+    updateItem: (text: string) => void
     editItem: (text: string) => void,
     Wrapper?: any
 }
 
 
 
-const EditableItem = ({children, deleteItem, editItem, initialText, Wrapper}: EditableItemProps) => {
+const EditableItem = ({deleteItem, editItem, updateItem, initialText, Wrapper}: EditableItemProps) => {
     const [isEdit, setIsEdit] = useState(false)
     const test = useRef<HTMLSpanElement>(null)
 
@@ -24,35 +25,79 @@ const EditableItem = ({children, deleteItem, editItem, initialText, Wrapper}: Ed
         const text = test.current && (test.current.innerText)
         if(text === null) return
         editItem(text)
-        setIsEdit(false)
+        // setIsEdit(false)
     }
 
     useEffect(() => {
         // test?.current?.focus()
+        // const onExit = (e: any) => {
+        //     // console.log('blur')
+        //     console.log(test?.current?.innerText || 'jopa -----------');
+        //     e.preventDefault()
+        //     e.stopPropagation()
+        //     updateItem(test?.current?.innerText || 'jopa -------------') 
+        //     document.removeEventListener('focusout', e)
+        //     // console.log(object);
+            
+        // }
+
+        // const onFocus = () => {
+        //     // console.log('focus');
+        //     // document.addEventListener('focusout', onExit)
+        //     if(test.current === document.activeElement){
+        //         console.log(test.current)
+        //         document.addEventListener('focusout', (e) => onExit(e))
+        //     }   
+        // }
+        // // console.log(test.current, document.activeElement);
+        // document.addEventListener('focus', onFocus)
+        // // if(test.current === document.activeElement){
+        // //     console.log(test.current)
+        // //     document.addEventListener('focusout', (e) => onExit)
+        // // }   
+        
         test.current && (test.current.innerText = initialText)
+
+        // return () => {
+            
+        //     document.removeEventListener('focus', onFocus)
+        // }
     })
 
-    return (
-        !isEdit
-            ?  
-            <EditableItemContainer>
-                    <Wrapper>
-                            {children}
-                            <EditButton onClick={handleIsEdit}/>
-                    </Wrapper>
-                    </EditableItemContainer>
 
-            : 
+    const handleClick = () => {
+        console.log('handle click');
+         const onExit = (e: any) => {
+            updateItem(test?.current?.innerText || '') 
+            document.removeEventListener('focusout', onExit)            
+        }
+        document.addEventListener('focusout', onExit)
+    }
+
+    // const handle
+
+    return (
+        // !isEdit
+        //     ?  
+        //     <EditableItemContainer onClick={handleIsEdit}> 
+        //             <Wrapper>
+        //                     {children}
+        //                     {/* <EditButton onClick={handleIsEdit}/> */}
+        //             </Wrapper>
+        //     </EditableItemContainer>
+
+        //     : 
         
-        <EditableItemContainer>
-                    {/* <CardContainer> */}
-                    <Wrapper>
-                        <ResizableTextArea ref={test}/>
-                        <EditButton style={{backgroundColor: 'red'}} onClick={deleteItem}/>
-                    </Wrapper>
-                    {/* </CardContainer> */}
-            <SubmitEditButton onClick={handleEdit}>submit</SubmitEditButton>
-        </EditableItemContainer>   
+        // <EditableItemContainer>
+                    /* <CardContainer> */
+                    <span onClick={handleClick}>
+                        <ResizableTextArea ref={test}
+                         onChange={() => {}}/>
+                        {/* <EditButton style={{backgroundColor: 'red'}} onClick={deleteItem}/> */}
+                    </span>
+                    /* </CardContainer> */
+            /* <SubmitEditButton onClick={handleEdit}>submit</SubmitEditButton> */
+        // </EditableItemContainer>   
 
    
         
