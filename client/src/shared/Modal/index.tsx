bec037ce7df007333f5d6686faa1012f5b40f2c9
Nikita <niksone123454321@gmail.com>
2021-04-08 22:32:37 +0300

    upd modal(add portal,handleExit,imperativeHandle)

--- client/src/shared/Modal/index.tsx
+++ client/src/shared/Modal/index.tsx
@@ -1,35 +1,67 @@
-import React, { Children, useEffect } from 'react'
+import React, { Children, forwardRef, Ref, useEffect, useImperativeHandle, useState } from 'react'
+import { createPortal } from 'react-dom';
 import { ModalContainer, ModalContent, ModalWrapper } from './ModalElements'
 
-const Modal = ({children, exit}: any) => {
+const modalElement = document.getElementById('modal-root');
 
+
+const Modal = ({children, exit, show = false}: any, ref: Ref<any>) => {
+
+    const [isOpen, setIsOpen] = useState(show);
+
+    const handleExit = () => {
+        setIsOpen(false)
+        exit && exit()
+    }
     useEffect(() => {
         const modalListener = (e: any) => {
-            // console.log(e.key)
             if(e.key === 'Escape'){
-                exit()
+                handleExit()
             }
         }
-        document.addEventListener('keydown', e => modalListener(e))
+        document.addEventListener('keydown', modalListener)
 
         return () => {
-            document.removeEventListener('keydown',e => modalListener(e))
+            document.removeEventListener('keydown', modalListener)
         }
     })
 
+    useImperativeHandle(
+        ref,
+        () => ({
+            open: () => setIsOpen(true),
+            close: () => {
+                handleExit()
+                console.log('close')
+            },
+        }),
+        [],
+    )
+
     return (
-        <ModalContainer >
-            {/* <div style={{display: 'block', width: '100%', height: '100%'}}> */}
-                <ModalWrapper onClick={exit}>
+        modalElement && 
+        createPortal(
+            <ModalContainer show={isOpen} >
+                {/* <div style={{display: 'block', width: '100%', height: '100%'}}> */}
+                    <ModalWrapper onClick={handleExit}>
 
-                </ModalWrapper>
-                <ModalContent>
-                    {children}
-                </ModalContent>
-            {/* </div> */}
+                    </ModalWrapper>
+                    <ModalContent onClick={e => e.stopPropagation()}>
+                        {children}
+                    </ModalContent  >
+                {/* </div> */}
 
-        </ModalContainer>
+            </ModalContainer>
+            ,
+            modalElement
+        )
+
     )
 }
 
-export default Modal
+const exportModal =  forwardRef(Modal)
+
+export {exportModal as Modal}
+
+export type ModalHandle = React.ElementRef<typeof exportModal>;
+
