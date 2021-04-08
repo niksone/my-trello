c515d6891e59281067044fe2f9b0bf388b4d9e0b
Nikita <niksone123454321@gmail.com>
2021-04-08 22:42:49 +0300

    update card structure

--- client/src/shared/Card/BeautifulCard.tsx
+++ client/src/shared/Card/BeautifulCard.tsx
@@ -1,54 +1,125 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import { Draggable } from 'react-beautiful-dnd'
 import { useDispatch, useSelector } from 'react-redux'
-import { deleteTask, updateTaskText } from '../../redux/AddItem/actionCreators'
-import { Task } from '../../redux/AddItem/interfaces'
+import { deleteCard, updateCard } from '../../redux/AddItem/actionCreators'
+import { Card, SimpleCard, Task } from '../../redux/AddItem/interfaces'
 import { RootReducerType } from '../../redux/store'
 import EditableItem from '../EditableItem'
-import { CardContainer } from './CardElements'
-
+import ProgressBar from '../ProgressBar'
+import { CardProgressContainer, CardContainer, CardContainerBlock, CardDescription, CardSubtitle, CardTitle, CardTitleContainer, CardTitleWrapper, CardProgressInfo, CardProgressLabel, CardProgressStage, CardProgressIcon } from './CardElements'
+import {ReactComponent as ChecklistIcon} from '../icons/tasks.svg'
+import Button from '../Buttons'
+import {ReactComponent as MoreIcon} from '../icons/more.svg'
+import {Modal, ModalHandle} from '../Modal'
+import CardForm from '../Column/AddCardForm'
 
 interface CardPropsI {
-    taskId: string,
-    task: Task,
+    cardId: string,
+    card: Card,
     listId: string
 }
 
-const BeautifulCard = ({taskId, task, listId}: CardPropsI) => {
-  const {taskIds} = useSelector((state: RootReducerType) => state.addItem)
+export const getCompletedTasks = (tasks: Task[]) => {
+  // console.log('completed tasks - ' + JSON.stringify(tasks.filter(task => task.completed === true)));
+  return tasks.filter(task => task.completed === true).length
+}
+
+const BeautifulCard = ({cardId, card, listId}: CardPropsI) => {
+  const {cardIds} = useSelector((state: RootReducerType) => state.addItem)
   const boardId = useSelector((state: RootReducerType) => state.addItem)._id
-  const index = taskIds.findIndex(task => task === taskId)
-  const [isEdit, setIsEdit] = useState(false)
+  const index = cardIds.findIndex(card => card === cardId)
+  const [showModal, setShowModal] = useState(false)
   const dispatch = useDispatch()
 
+  const modalRef = useRef<ModalHandle>(null)
+
   const editCard = (text: string) => {
-    dispatch(updateTaskText(boardId, listId, taskId, text))
+    // dispatch(updateTaskText(boardId, listId, taskId, text))
+    // dispatch(updateCard(boardId, listId, cardId, card))
   }
 
   const handleDeleteCard = () => {
-    dispatch(deleteTask(boardId, listId, taskId))
+    dispatch(deleteCard(boardId, listId, cardId))
   }
 
-  const handleEdit = () => {
-    setIsEdit(!isEdit)
+  // const handleEdit = () => {
+  //   setIsEdit(!isEdit)
+  // }
+
+  const handleSave = (savedCard: SimpleCard) => {
+    console.log('handlesave');
+    dispatch(updateCard(boardId, listId, cardId, {_id: card._id, ...savedCard}))
   }
 
-    return (
-        <Draggable draggableId={taskId} index={index} key={taskId}>
+  const [completedTasks, allTasks] = [getCompletedTasks(card.tasks), card.tasks.length]
+  
+  // console.log(completedTasks, allTasks);
+
+    return ( 
+        <Draggable draggableId={cardId} index={index} key={cardId}>
         {(provided, snapshot) => (
           <div
           ref={provided.innerRef}
           {...provided.dragHandleProps}
           {...provided.draggableProps}
         > 
-            <EditableItem
+            {/* <EditableItem
               deleteItem={handleDeleteCard}
               editItem={editCard}
-              initialText={task.text}
+              initialText={card.title}
               Wrapper={CardContainer}
-            >
-                {task.text}
-            </EditableItem>
+            > */}
+            <CardContainer onClick={() => setShowModal(true)}>
+              <CardContainerBlock>
+                <CardTitleContainer>
+                  <CardTitleWrapper>
+                    <CardTitle>{card.title}</CardTitle>
+                    <CardSubtitle>{card.subtitle}</CardSubtitle>
+                  </CardTitleWrapper>
+                  <Button shape='icon' variant='outline' size='lg'>
+                    <MoreIcon />
+                  </Button>
+                </CardTitleContainer>
+              </CardContainerBlock>
+              {card.description !== '' &&
+                <CardContainerBlock>
+                  <CardDescription>
+                    {card.description}
+                  </CardDescription>
+                </ CardContainerBlock>
+              }
+
+              {card.tasks.length > 0 &&
+                <CardContainerBlock>
+                  <CardProgressContainer>
+                    <CardProgressInfo>
+                      <CardProgressLabel>
+                        <CardProgressIcon><ChecklistIcon/></CardProgressIcon>
+                        Checklist
+                      </CardProgressLabel>
+                      <CardProgressStage> {completedTasks} / {allTasks}</CardProgressStage>
+                    </CardProgressInfo>
+                    <ProgressBar variant='default' value={completedTasks / allTasks * 100}/>
+                  </CardProgressContainer>
+                </ CardContainerBlock>
+              }
+            </CardContainer>
+            {showModal &&
+              <Modal ref={modalRef} show={showModal} exit={() => setShowModal(false)}>
+                <CardForm 
+                  columnId={listId}
+                  cardId={card._id}
+                  title={card.title}
+                  subtitle={card.subtitle}
+                  description={card.description}
+                  tasks={card.tasks}
+                  onExit={() => modalRef.current.close()}
+                  onSave={(savedCard: SimpleCard) => handleSave(savedCard)}
+                  
+                />
+              </Modal>
+            }
+            {/* </EditableItem> */}
           </div>
         )}
       </Draggable>
