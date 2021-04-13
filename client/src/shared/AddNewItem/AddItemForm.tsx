11896484166bfc6eaae245296dcad14e9c73ba00
Nikita <niksone123454321@gmail.com>
2021-04-13 22:16:23 +0300

    add different variants

--- client/src/shared/AddNewItem/AddItemForm.tsx
+++ client/src/shared/AddNewItem/AddItemForm.tsx
@@ -1,19 +1,23 @@
 import { useRef } from 'react'
 import Button from '../Buttons'
 import ResizableTextArea from '../ResizableTextArea'
-import { AddItemContainerTypes, AddItemFormButton, AddItemFormContainer,AddItemFormWrapper } from './AddNewItemElements'
+import { AddItemContainerTypes, AddItemFormButton, AddItemFormContainer,AddItemFormWrapper, FormIcon } from './AddNewItemElements'
+
+type FormVariant = 'icon' | 'default'
 
 
 interface AddItemFormProps {
     title: string,
     placeholder?: string
     onAdd(text: string): void
-    item?: AddItemContainerTypes
+    item?: AddItemContainerTypes,
+    Icon?: any
+    variant?: FormVariant
 }
 
-const AddItemForm = ({title, placeholder, item, onAdd}: AddItemFormProps) => {
+const AddItemForm = ({title, placeholder, item, onAdd, Icon, variant = 'default'}: AddItemFormProps) => {
     const test = useRef<HTMLSpanElement>(null)
-    
+    // const FormIcon = Icon
     const handleAddItem = () => {
         const text = test?.current?.innerText || '';
         console.log(text);
@@ -22,11 +26,40 @@ const AddItemForm = ({title, placeholder, item, onAdd}: AddItemFormProps) => {
         test.current && (test.current.innerText = "")
     }
 
+    const getForm = (variant: FormVariant) => {
+        switch(variant){
+            case 'default': {
+                return (
+                    <AddItemFormContainer>
+                        <AddItemFormWrapper item={item}>
+                            <ResizableTextArea ref={test} placeholder={placeholder}/>
+                        </AddItemFormWrapper>
+                        <Button onClick={handleAddItem} size='md'>{title}</Button>
+                    </AddItemFormContainer>
+                )
+            }
+
+            case 'icon': {
+                return (
+                    <AddItemFormContainer fd='row' alignItems='center'>
+                        {Icon && 
+                            <FormIcon onClick={handleAddItem}>
+                                <Icon />
+                            </FormIcon> 
+                        }
+                        <AddItemFormWrapper item={item}>
+                            <ResizableTextArea ref={test} placeholder={placeholder}/>
+                        </AddItemFormWrapper>
+                    </AddItemFormContainer>
+                )
+            }
+
+
+        }
+    }
+
     return (
-        <AddItemFormContainer >
-            <AddItemFormWrapper item={item}><ResizableTextArea ref={test} placeholder={placeholder}/></AddItemFormWrapper>
-            <Button onClick={handleAddItem} size='md'>{title}</Button>
-        </AddItemFormContainer>
+        getForm(variant)
     )
 }
 
