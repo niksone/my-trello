0eb7242c70923f2048c87470f6f260c9a1e86da2
Nikita <niksone123454321@gmail.com>
2021-04-14 01:10:18 +0300

    add api change to delete / edit board

--- client/src/redux/Board/actionCreators.ts
+++ client/src/redux/Board/actionCreators.ts
@@ -13,18 +13,18 @@ export const getBoards = (user: string) => async (dispatch: Dispatch<BoardAction
 
 export const addBoard = (userId: string, boardName: string) => async (dispatch: Dispatch<BoardAction>) => {
     try {
-        // const board = (await boardApi.addBoard(userId, boardName)).data
-        // dispatch({type: 'ADD_BOARD', payload: {id: board._id, name: boardName}})
-        dispatch({type: 'ADD_BOARD', payload: {id: new Date().toISOString(), name: boardName}})
+        const board = (await boardApi.addBoard(userId, boardName)).data
+        dispatch({type: 'ADD_BOARD', payload: {id: board._id, name: boardName}})
+        // dispatch({type: 'ADD_BOARD', payload: {id: new Date().toISOString(), name: boardName}})
     } catch (error) {
         console.log(error)
     }
 }
 
-export const deleteBoard = (id: string) => async (dispatch: Dispatch<BoardAction>) => {
+export const deleteBoard = (userId:string, boardId: string) => async (dispatch: Dispatch<BoardAction>) => {
     try {
-        console.log(`delete board id ${id}`)
-        dispatch({type: 'DELETE_BOARD', payload: {id}})
+        dispatch({type: 'DELETE_BOARD', payload: {id: boardId}})
+        await boardApi.deleteBoard(userId, boardId)
     } catch (error) {
         console.log(error)
     }
@@ -33,6 +33,7 @@ export const deleteBoard = (id: string) => async (dispatch: Dispatch<BoardAction
 export const editBoardName= (id: string, name: string) => async (dispatch: Dispatch<BoardAction>) => {
     try {
         dispatch({type: 'EDIT_BOARD_NAME', payload: {id, name}})
+        await boardApi.editBoardName(id, name)
     } catch (error) {
         console.log(error)
     }
