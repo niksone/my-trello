84f0e0ff04b92eb6c84f597f4e0632c2d0d6a468
Nikita <niksone123454321@gmail.com>
2021-04-25 02:07:51 +0300

    tooltip fixed pos

--- client/src/shared/Tooltip/index.tsx
+++ client/src/shared/Tooltip/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import { TooltipCloseContainer, TooltipContainer, TooltipContent } from './TooltipElements'
 
 interface TooltipProps{ 
@@ -8,26 +8,37 @@ interface TooltipProps{
 
 const Tooltip = ({content, children, direction}: React.PropsWithChildren<TooltipProps>) => {
     const [show, setShow] = useState(false)
+    const tooltipRef = useRef<HTMLDivElement>(null)
+    const tooltipContentRef = useRef<HTMLDivElement>(null)
+
+    const [{offsetX, offsetY}, setOffset] = useState({offsetX: 0, offsetY: 0})
 
     const handleClick = (e: any) => {
         e.stopPropagation()
+        const bodyRect = document.body.getBoundingClientRect(),
+              elemRect = tooltipRef.current?.getBoundingClientRect(),
+              offsetY = elemRect && (elemRect.bottom + 7) || 0,
+              offsetX = elemRect && (window.innerWidth - elemRect.right) || 0
+        
+        setOffset(prev => ({offsetX, offsetY}) || prev) 
+        console.log(offsetX, offsetY);  
         setShow(prev => !prev)
     }
 
     return (
         <TooltipContainer 
             onClick={handleClick}
+            ref={tooltipRef}
         >
             {children}
             {
                 show && (
                     <>
-                    <TooltipCloseContainer onClick={handleClick}>
-
+                    <TooltipCloseContainer onTouchStart={handleClick}>
+                        <TooltipContent offsetX={offsetX} offsetY={offsetY}>
+                            {content}
+                        </TooltipContent>
                     </TooltipCloseContainer>
-                    <TooltipContent>
-                        {content}
-                    </TooltipContent>
                 </>
                 )
             }   
