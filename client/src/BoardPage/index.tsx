ab3ada04923978fd52917171e0fb2c5fbee76400
Nikita <niksone123454321@gmail.com>
2021-05-01 18:09:37 +0300

    change overflow

--- client/src/BoardPage/index.tsx
+++ client/src/BoardPage/index.tsx
@@ -1,19 +1,12 @@
-import React, { useContext, useEffect, useMemo, useRef, useState } from 'react'
+import { useContext, useEffect} from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { useParams } from 'react-router-dom'
-import { useSwipeable } from 'react-swipeable'
-import { ThunkAction, ThunkDispatch } from 'redux-thunk'
 import styled from 'styled-components'
-import { authApi } from '../api'
 import { userContext } from '../Context'
 import { setBoard } from '../redux/AddItem/reducer'
-import { getBoards } from '../redux/Board/actionCreators'
 import { Board } from '../redux/Board/interfaces'
 import { RootReducerType, store } from '../redux/store'
 import BeautifulBoard from '../shared/Board/BeautifulBoard'
-import Header from '../shared/Header'
-import { ModalHandle } from '../shared/Modal'
-import { useFetching } from '../utils/useFetching'
 
 export const AppContainer = styled.div`
   height: 100%;
@@ -25,7 +18,7 @@ export const BoardSectionWrapper = styled.div`
     width: 100%;
     background-color: #fff;
     border-radius: 12px;
-    overflow-x: auto;
+    overflow: auto;
     @media screen and (max-width: 425px){
         border-radius: 0;
         background-color: var(--color-background-light);
@@ -53,10 +46,7 @@ const BoardPage = () => {
         dispatch(setBoard(currentBoard))
     }, [dispatch, currentBoard._id, user])
     return (
-            <BeautifulBoard data={data} />
-        // </BoardSectionWrapper>
-            
-        // </AppContainer>
+        <BeautifulBoard data={data} />
     )
 }
 
