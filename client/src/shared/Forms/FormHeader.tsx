cdf0d6b7ddaed44860724221a4c11835cea9b053
Nikita <niksone123454321@gmail.com>
2021-05-04 18:59:26 +0300

    create FormHeader

---
+++ client/src/shared/Forms/FormHeader.tsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import ConditionalWrapper from '../ConditionalWrapper'
+import EditableItem from '../EditableItem'
+import { FormContainer, FormSubtitle, FormTitle, FormTitleContainer } from './FormElements'
+
+interface FormHeaderProps {
+    title: string,
+    handleTitleUpdate?: (text: string) => void
+    subtitle: string
+    handleSubtitleUpdate?: (text: string) => void
+    editable?: boolean
+
+}
+
+const FormHeader = ({title, handleTitleUpdate, subtitle, handleSubtitleUpdate, editable = false}: FormHeaderProps) => {
+    console.log(subtitle, title);
+    return (
+        <FormTitleContainer>
+            <FormContainer>
+            <FormTitle>
+                {
+                    editable 
+                    ?
+                        <EditableItem
+                            initialText={title}
+                            deleteItem={() => {}}
+                            editItem={() => {}}
+                            placeholder='Enter Title'
+                            updateItem={(text) => handleTitleUpdate && handleTitleUpdate(text)}
+                        />
+                    : title
+                }
+            </FormTitle>
+            <FormSubtitle>
+                {
+                    editable 
+                    ?
+                    <EditableItem
+                        initialText={subtitle}
+                        deleteItem={() => {}}
+                        editItem={() => {}}
+                        placeholder='Enter subtitle'
+                        updateItem={(text) => handleSubtitleUpdate && handleSubtitleUpdate(text)}
+                    />
+                    : subtitle
+                }   
+            </FormSubtitle>
+            </FormContainer>
+        </FormTitleContainer>
+    )
+}
+
+export default FormHeader
