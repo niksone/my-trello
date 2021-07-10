f732d9833e501e1ff755871a2a3a4650c57a0808
Nikita <niksone123454321@gmail.com>
2021-07-11 01:20:52 +0300

    implement react lazy

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,17 @@
-import React, { useContext, useEffect, useRef } from 'react';
+import React, { Suspense, useContext, useEffect, useRef } from 'react';
 import { GlobalStyles } from './globalStyles';
 import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
-import RegisterPage from './Auth/RegisterPage';
-import BoardPage from './BoardPage';
-import LoginPage from './Auth/LoginPage';
+// import RegisterPage from './Auth/RegisterPage';
+// import BoardPage from './BoardPage';
+// import LoginPage from './Auth/LoginPage';
 import UserContext, { userContext } from './Context';
 import ProtectedRoute from './ProtectedRoute';
-import HomePage from './HomePage';
+// import HomePage from './HomePage';
 import Div100vh from './shared/Div100vh';
+
+const HomePage = React.lazy(() => import('./HomePage'))
+const RegisterPage = React.lazy(() => import('./Auth/RegisterPage'))
+const LoginPage = React.lazy(() => import('./Auth/LoginPage'))
 
 
 function App() {
@@ -18,11 +22,13 @@ function App() {
         <UserContext>
             <Router>
             <Switch>
-              <ProtectedRoute path='/board' exact component={() => <HomePage />} />
-              <ProtectedRoute path='/board/:id' children={() => <HomePage />} />
-              <ProtectedRoute auth path='/register' component={() => <RegisterPage />} />
-              <ProtectedRoute auth path='/login' component={() => <LoginPage />} />
-              <Redirect to='/board' />
+              <Suspense fallback={<div>suspense</div>} >
+                <ProtectedRoute path='/board' exact component={() => <HomePage />} />
+                <ProtectedRoute path='/board/:id' children={() => <HomePage />} />
+                <ProtectedRoute auth path='/register' component={() => <RegisterPage />} />
+                <ProtectedRoute auth path='/login' component={() => <LoginPage />} />
+                <Redirect to='/board' />
+              </ Suspense>
             </Switch>
             </Router>
         </UserContext>
