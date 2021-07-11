c910a3f372246b6ca486dbbfc380aee4da160421
Nikita <niksone123454321@gmail.com>
2021-07-12 01:14:37 +0300

    create loader for suspense

---
+++ client/src/shared/Loaders/AppLoader.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import styled from 'styled-components'
+import RingLoader from './RingLoader'
+
+export const AppLoaderContainer = styled.div`
+    height: 100%;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+
+const AppLoader = () => {
+    const size = window.innerWidth > window.innerHeight ? '50vh' : '50vw'
+
+    return (
+        <AppLoaderContainer>
+            <RingLoader size={size}/>
+        </AppLoaderContainer>
+    )
+}
+
+export default AppLoader
