d42d3994ffd2a5a509a8ac3118654644760f44a0
Nikita <niksone123454321@gmail.com>
2021-07-16 16:26:44 +0300

    create constants file

---
+++ client/src/shared/contants.ts
@@ -0,0 +1,18 @@
+export const COLORS = {
+    primary: 'var(--color-primary)',
+    primaryDark: 'var(--color-primary-dark)',
+    primaryGrey: 'var(--color-primary-grey)',
+    primaryLight: 'var(--color-primary-light)',
+    outline: 'var(--color-outline)',
+    error: 'var(--color-error)',
+    errorHover: 'var(--color-error-hover)',
+    errorLight: 'var(--color-error-light)',
+    warning: 'var(--color-warning)',
+    success: 'var(--color-success)',
+    backgroundLight: 'var(--color-background-light)',
+    restingOutline: 'var(--color-resting-outline)',
+    buttonHover: 'var(--color-button-hover)',
+    buttonOutlineResting: 'var(--color-button-outline-resting)',
+    background: 'var(--color-background)',
+    borderLight: 'var(--color-border-light)',
+}
